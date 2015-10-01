9b23f9ab876550fa4b46e5cc3abb1cac9cb88e50
0911707 <0911707@hr.nl>
2015-10-01 16:41:23 +0200

    Julian in't Veld 0911707

--- PythonApplication2/PythonApplication2/RPS.py
+++ PythonApplication2/PythonApplication2/RPS.py
@@ -1,47 +1,45 @@
-from random import randint
+﻿from random import randint
 while True:
-    choice = raw_input("Do you choose rock, paper or scissors? (press Q to quit) ")
+    choice = raw_input("Do you choose rock, paper or scissors? (press Q to quit) \n")
     computer = randint(0, 2)
+    if computer == 0:
+        print ("The computer chooses scissors\n")
+        x = "scissors"
+    elif computer == 1:
+        print ("The computer chooses rock\n")
+        x = "rock"
+    else:
+        print ("The computer chooses paper\n")
+        x = "paper"
+
+    if choice == "rock":
+        y = "rock"
+    elif choice =="paper":
+        y = "paper"
+    else:
+        y = "scissors"
+
     if (choice == "q") or (choice == "Q"):
         break
+    
+    if (choice == "rock" and computer == 0) or (choice == "scissors" and computer == 2) or (choice == "paper" and computer == 1):
+        print(y + " beats " + x  + ", you win!\n")
+    elif (choice == "rock" and computer == 1) or (choice == "scissors" and computer == 0) or (choice == "paper" and computer == 2):
+        print(x + " ties with " + y + ", it's a tie\n")
+    else:
+        print(x + " beats " + y + ", you lose\n")
+
+
+    
+    
+    
+    
+
+    
+    
+    
+    
         
-    if (choice == "rock") and (computer == 0):
-        print("")
-        print("The computer chooses scissors, rock beats scissors, you win!")
-        print("")
-    elif(choice =="rock") and (computer == 1):
-        print("")
-        print ("The computer also chooses rock, it's a tie")
-        print("")
-    elif(choice =="rock") and (computer == 2):
-        print("")
-        print ("The computer chooses paper, paper beats rock, you lost from a inanimate object, N00B!")
-        print("")
 
-    elif(choice =="scissors") and (computer == 0):
-        print("")
-        print ("The computer also chooses scissors, it's a tie")
-        print("")
-    elif(choice =="scissors") and (computer == 1):
-        print("")
-        print ("The computer chooses rock, rock beats scissors, you lost from a inanimate object, N00B!")
-        print("")
-    elif(choice =="scissors") and (computer == 2):
-        print("")
-        print ("The computer chooses paper, scissors beats paper, you win!")
-        print("")
-
-    elif(choice =="paper") and (computer == 0):
-        print("")
-        print ("The computer chooses scissors, scissors beats paper, you lost from a inanimate object, N00B!")
-        print("")
-    elif(choice =="paper") and (computer == 1):
-        print("")
-        print ("The computer chooses rock, paper beats rock, you win!")
-        print("")
-    elif(choice =="paper") and (computer == 2):
-        print("")
-        print ("The computer chooses paper, it's a tie")
-        print("")
 
 
