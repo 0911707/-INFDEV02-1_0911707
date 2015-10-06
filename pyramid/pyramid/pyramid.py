bc30ad4b837a673f5d0ad5894703881d7a806563
0911707 <0911707@hr.nl>
2015-10-06 20:56:44 +0200

    finished pyramid Julian in't Veld 0911707

---
+++ pyramid/pyramid/pyramid.py
@@ -0,0 +1,25 @@
+﻿input = raw_input("how many layers does the pyramid need to have? ")
+x = 0
+output = ""
+
+try:
+    x = int(input)
+except ValueError:
+    print("you did not enter a number!")
+
+counter = x
+xcount = 1
+
+for i in range(x):
+    for j in range(counter):
+        output += " "
+    for k in range(xcount):
+        output += "*"
+    output += "\n"
+    
+    if counter >= 0:
+        counter -= 1
+    if xcount <= x + x:
+        xcount += 2
+
+print(output)
