022ecbbfaabbf41dd1fc68b2996cd7545ec5757c
0911707 <0911707@hr.nl>
2015-10-07 07:31:53 +0200

    made it a filled circle instead of hollow one

--- filled circle/filled circle/filled_circle.py
+++ filled circle/filled circle/filled_circle.py
@@ -1,18 +1,19 @@
-import math
+﻿import math
 input = raw_input("what is the radius of the circle? ")
 print("\n")
 
+rad = 0
 try:
     rad = int(input)
 except ValueError:
     print("you did not enter a number")
 
 output = ""
-rad = 0
+
 
 for i in range(-rad, rad + 1):
     for j in range(-rad, rad + 1):
-        if float(math.sqrt(i**2 + j**2) >= rad -0.5) and float(math.sqrt(i**2 + j**2) <= rad + 0.5):
+        if float(math.sqrt(i**2 + j**2) <= rad + 0.5):
             output += "*"
         else:
             output += " "
